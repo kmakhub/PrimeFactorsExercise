73723d44d202b63e7b9c223fbe004cd9a3d3b5c2
Your Name <kulanimakhubele@Kulanis-MacBook-Pro.local>
2015-04-16 17:21:57 +0200

    Prime factors working fine now

--- src/PrimeFactors.java
+++ src/PrimeFactors.java
@@ -2,32 +2,34 @@ import com.sun.tools.javac.util.List;
 
 import java.util.ArrayList;
 
-/**
- * Created by kulani makhubele on 15/04/15.
- */
 public class PrimeFactors {
 
     public static  void main(String[] args) {
 
-        ArrayList<Integer> listArray = generate(4);
+                generate(16);
+    }
 
-        for (int i = 0; i < listArray.size(); i++)
-        {
-            System.out.print(i+",");
+    public static int factor(int n) {
+        int f = n;
+        for (int i = 2; i < n; i++) {
+            if (n % i == 0) {
+                f = i;
+                break;
+            }
+        }
+        return f;
+    }
+
+    public static void generate(int n){
+
+        if (n > 1) {
+            factor(n);
+            System.out.println(factor(n));
+            generate(n / factor(n));
         }
     }
-
-    public static ArrayList<Integer> generate(int n){
-
-        ArrayList<Integer> listArray = new ArrayList<Integer>();
-            for (int i = 2; i < n; i++) {
-                if( n % i == 0) {
-                    System.out.print(i+",");
-                    listArray.add(i);
-                }
-            }
-
-       return listArray;
-
-    }
 }
+
+
+
+
